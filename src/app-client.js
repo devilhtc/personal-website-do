991b885e9149f3c773bcf9594be37f78822e4463
devilhtc <devilmengsk@gmail.com>
2017-10-10 21:06:06 -0700

    .

--- src/app-client.js
+++
@@ -1,8 +0,0 @@
-// src/app-client.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TrialComponent from './components/TrialComponent';
-
-window.onload = () => {
-  ReactDOM.render(<TrialComponent/>, document.getElementById('main'));
-};
---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react' 
+import ReactDOM from 'react-dom'
+import TrialComponent from 'components/TrialComponent'
+
+ReactDOM.render(<TrialComponent/>,  document.getElementById("app"));
