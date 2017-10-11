f8092c4a52ee8e98350d40afd843a3a6dfdd3733
devilhtc <devilmengsk@gmail.com>
2017-10-10 21:33:24 -0700

    .

--- src/app.js
+++ src/app.js
@@ -2,4 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import TrialComponent from 'components/TrialComponent'
 
+module.hot.accept();
+
 ReactDOM.render(<TrialComponent/>,  document.getElementById("app"));
