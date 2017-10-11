6d4d22cd685ed998cf9b95df022cce276c98df8e
devilhtc <devilmengsk@gmail.com>
2017-10-10 20:26:00 -0700

    added store

--- src/components/TrialComponent.js
+++ src/components/TrialComponent.js
@@ -1,10 +1,17 @@
 // src/components/TrialComponent.js
 import React from 'react';
+import store from '../../store/mainStore.js';
 
 export default class TrialComponent extends React.Component {
 	render() {
 		return (
-			<h1> Simple Trial </h1>
+			<div>
+				<h1> Simple Trial {store.getState()}</h1>
+				<button onclick={store.dispatch({type:'INC'})}> increment </button>
+				<button onclick={store.dispatch({type:'DEC'})}> decrement </button>
+			</div>
 		);
 	}
+
+
 }
---
+++ store/mainStore.js
@@ -0,0 +1,20 @@
+import { createStore } from 'redux'
+
+function counter(state = 0, action) {
+	switch (action.type) {
+		case 'INC':
+			return state + 1;
+		case 'DEC':
+			return state - 1;
+		default:
+			return state;
+	}
+}
+
+let store = createStore(counter);
+
+store.subscribe(()=>
+	console.log(store.getState())
+);
+
+export store;
