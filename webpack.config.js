23ee5ef29b5b3b72cc4ea0b8151621f1fd0ee991
devilhtc <devilmengsk@gmail.com>
2017-10-10 21:19:57 -0700

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,24 @@
-var path = require('path');
-var webpack = require('webpack');
- 
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/app.html',
+  filename: 'app.html',
+  inject: 'body'
+})
 module.exports = {
-     entry: './js/app.js',
+     entry: './src/app.js',
      output: {
-         path: path.resolve(__dirname, 'build'),
+         path: './bin',
          filename: 'app.bundle.js'
      },
-     module: {
-         loaders: [
-             {
-                 test: /\.js$/,
-                 loader: 'babel-loader',
-                 query: {
-                     presets: ['es2015', 'react']
-                 }
-             }
-         ]
-     },
-     stats: {
-         colors: true
-     },
-     devtool: 'source-map'
-};
+    module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  }
+ };
+
+ plugins: [HtmlWebpackPluginConfig]
+}
